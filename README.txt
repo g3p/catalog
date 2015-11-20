37a7e05d2fc7a65ec4a68fdad6141ef1382ac0e8
g <g@g.com>
2015-11-20 10:52:21 +0100

    second submission

--- README.txt
+++ README.txt
@@ -1,1 +1,48 @@
-Place your catalog project in this directory.
+Item Catalog app
+
+-Intro
+Catalog is an application that provides a list of items within a variety of categories. Catalog provides a user registration
+and authentication system. Registered users have the ability to post, edit and delete their own items.
+
+-Set Up Environment
+The VM is a Linux server system that runs on top of your own computer.
+We're using the Vagrant software to configure and manage the VM. Here are the tools you'll need to install to get it running:
+
+Git
+
+If you don't already have Git installed, download Git from git-scm.com. Install the version for your operating system.
+
+On Windows, Git will provide you with a Unix-style terminal and shell (Git Bash). 
+(On Mac or Linux systems you can use the regular terminal program.)
+
+VirtualBox
+
+VirtualBox is the software that actually runs the VM. You can download it from virtualbox.org, here.
+Install the platform package for your operating system.  You do not need the extension pack or the SDK.
+
+Vagrant
+
+Vagrant is the software that configures the VM and lets you share files between your host computer and the VM's filesystem. 
+You can download it from vagrantup.com. Install the version for your operating system.
+
+Run the virtual machine!
+
+Using the terminal, change directory to fullstack/vagrant (cd fullstack/vagrant), then type vagrant up to launch your virtual machine.
+
+Once it is up and running, type vagrant ssh to log into it. This will log your terminal in to the virtual machine, and you'll get a Linux shell prompt.
+When you want to log out, type exit at the shell prompt.  To turn the virtual machine off (without deleting anything), type vagrant halt. 
+If you do this, you'll need to run vagrant up again before you can log into it. 
+Be sure to change to the /vagrant directory by typing cd /vagrant in order to share files between your home machine and the VM.  
+
+
+-Installation
+Unzip Catalog in /vagrant/Catalog folder
+
+-Set Up
+run database_setup.py to create the catalog database, and run lotsofitems.py to populate it.
+
+-How to run
+run python project.py
+
+-Usage
+open the browser to http://localhost:5000/ to explore the app
--- project.py
+++ project.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
+from flask import Flask, render_template, request,\
+ redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, SportItem
+from database_setup import Base, Category, SportItem, User
 from flask import session as login_session
 import random
 import string
@@ -24,6 +25,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
@@ -32,6 +34,7 @@ def showLogin():
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -49,7 +52,7 @@ def gconnect():
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
         login_session['credentials'] = credentials.access_token
-        print "1 ",credentials
+        print "1 ", credentials
     except FlowExchangeError:
         response = make_response(
             json.dumps('Failed to upgrade the authorization code.'), 401)
@@ -89,13 +92,13 @@ def gconnect():
     stored_gplus_id = login_session.get('gplus_id')
     print "stored_gplus_id", stored_gplus_id
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps('Current user'
+                                            'is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Store the access token in the session for later use.    
-	login_session['credentials'] = credentials.access_token
+    # Store the access token in the session for later use.
+    login_session['credentials'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
     # Get user info
@@ -115,7 +118,8 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius:'
+    '150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
@@ -147,7 +151,7 @@ def gdisconnect():
 
         response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
-        return response
+        return redirect(url_for('catalogMenu'))
     else:
         # For whatever reason, the given token was invalid.
         response = make_response(
@@ -155,93 +159,111 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
+
 @app.route('/categories.json')
 def categoriesJSON():
-    categories = session.query(Category).all()   
+    categories = session.query(Category).all()
     return jsonify(categories=[c.serialize for c in categories])
+
 
 @app.route('/items.json')
 def categoryItemsJSON():
-    items = session.query(SportItem).all()   
+    items = session.query(SportItem).all()
     return jsonify(items=[i.serialize for i in items])
+
 
 @app.route('/')
 @app.route('/catalog/')
 def catalogMenu():
-  categories = session.query(Category).order_by(asc(Category.name))
-  items = session.query(SportItem).order_by(desc(SportItem.id))
-  if 'username' not in login_session:
-      return render_template('catalog.html', categories = categories, items = items)	
-  else:
-    return render_template('catalog_logged_in.html', categories = categories, items = items)  
+    categories = session.query(Category).order_by(asc(Category.name))
+    items = session.query(SportItem).order_by(desc(SportItem.id))
+    if 'username' not in login_session:
+        return render_template('catalog.html',
+                               categories=categories, items=items)
+    else:
+        return render_template('catalog_logged_in.html',
+                               categories=categories, items=items)
 
 # Task 1: Create route for category here
 
+
 @app.route('/catalog/<int:category_id>/items/')
 def categoryMenu(category_id):
-	categories = session.query(Category).order_by(asc(Category.name))
-	items = session.query(SportItem).filter_by(category_id = category_id)
-	return render_template('category.html', categories = categories, items = items)	
+    categories = session.query(Category).order_by(asc(Category.name))
+    items = session.query(SportItem).filter_by(category_id=category_id)
+    return render_template('category.html', categories=categories, items=items)
 
 # Task 2: Create route for item description function here
 
+
 @app.route('/catalog/<int:category_id>/<int:item_id>/')
 def itemMenu(category_id, item_id):
-    item = session.query(SportItem).filter_by(id = item_id).one()
+    item = session.query(SportItem).filter_by(id=item_id).one()
     if 'username' not in login_session:
-        return render_template('description.html', item = item)
+        return render_template('description.html', item=item)
     else:
-        return render_template('description_logged_in.html', item = item)    
+        return render_template('description_logged_in.html', item=item)
 
 
 # Task 3: Create a route for edit item function here
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/edit/', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit/',
+           methods=['GET', 'POST'])
 def editItem(category_id, item_id):
-    editedItem= session.query(
-        SportItem).filter_by(id=item_id).one()
-    if request.method == 'POST':
-        if request.form['name']:
-            editedItem.name = request.form['name']
-            flash('Item Successfully Edited: %s' % editedItem.name)            
-        if request.form['description']:
-            editedItem.description = request.form['description']
-            flash('%s description Successfully Edited.' % editedItem.name)
-        return redirect(url_for('catalogMenu'))
-
+    editedItem = session.query(SportItem).filter_by(id=item_id).one()
+    if 'username' not in login_session:
+        return redirect(url_for('showLogin'))
     else:
-        return render_template('editItem.html', item=editedItem)    
+        if request.method == 'POST':
+            if request.form['name']:
+                editedItem.name = request.form['name']
+                flash('Item Successfully Edited: %s' % editedItem.name)
+            if request.form['description']:
+                editedItem.description = request.form['description']
+                flash('%s description Successfully Edited.' % editedItem.name)
+            return redirect(url_for('catalogMenu'))
+
+        else:
+            return render_template('editItem.html', item=editedItem)
 
 
 # Task 4: Create a route for delete item function here
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/delete/', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete/',
+           methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
     itemToDelete = session.query(
-        SportItem).filter_by(id=item_id).one()
-    if request.method == 'POST':
-        session.delete(itemToDelete)
-        flash('%s Successfully Deleted' % itemToDelete.name)
-        session.commit()
-        return redirect(url_for('categoryMenu', category_id=category_id))
+            SportItem).filter_by(id=item_id).one()
+    if 'username' not in login_session:
+        return redirect(url_for('showLogin'))
     else:
-        return render_template('deleteItem.html', item=itemToDelete)
-
+        if request.method == 'POST':
+            session.delete(itemToDelete)
+            flash('%s Successfully Deleted' % itemToDelete.name)
+            session.commit()
+            return redirect(url_for('categoryMenu', category_id=category_id))
+        else:
+            return render_template('deleteItem.html', item=itemToDelete)
 
 
 # Task 5: Create a route for edit item function here
 
 @app.route('/catalog/add/', methods=['GET', 'POST'])
 def addItem():
-    if request.method == 'POST':
-        newItem = SportItem(name=request.form['name'], description=request.form['description'], category_id=request.form['category'])
-        session.add(newItem)
-        flash('New Item %s Successfully Created' % newItem.name)
-        session.commit()
-        return redirect(url_for('catalogMenu'))
+    if 'username' not in login_session:
+        return redirect(url_for('showLogin'))
     else:
-    	categories = session.query(Category).order_by(asc(Category.name))    	
-    	return render_template('newItem.html', categories = categories)    
+        if request.method == 'POST':
+            newItem = SportItem(name=request.form['name'],
+                                description=request.form['description'],
+                                category_id=request.form['category'])
+            session.add(newItem)
+            flash('New Item %s Successfully Created' % newItem.name)
+            session.commit()
+            return redirect(url_for('catalogMenu'))
+        else:
+            categories = session.query(Category).order_by(asc(Category.name))
+            return render_template('newItem.html', categories=categories)
 
 
 if __name__ == "__main__":
